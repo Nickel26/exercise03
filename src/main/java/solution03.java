f2f653916831c73585838849e0d0c981a32583ad
Nick <lopes.nick0801@gmail.com>
2021-09-05 11:51:34 -0400

    initial

--- src/main/java/solution03.java
+++ src/main/java/solution03.java
@@ -7,12 +7,17 @@ public class solution03 {
      */
     private static final Scanner in = new Scanner(System.in);
     public static void main(String[] args){
+        //Prompt user for quote
+        //Store quote input in string
+        //Prompt user for author of quote
+        //Store author input in string
+        //Display output prompt with author and quote strings
         System.out.print("What is the quote?");
         String quote = in.nextLine();
 
         System.out.print("Who said it?");
         String author = in.nextLine();
 
-        System.out.printf(author + " says, \"" + quote + "\"");
+        System.out.print(author + " says, \"" + quote + "\"");
     }
 }
